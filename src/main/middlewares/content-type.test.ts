c1a094b21c5d24eafb9035233055d0b5b02da1b1
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-14 17:18:01 -0300

    test: ensure ContentTypeMiddleware returns xml content type when forced

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -9,4 +9,14 @@ describe('ContentTypeMiddleware', () => {
 
     expect(response.headers['content-type']).toMatch(/json/);
   });
+
+  it('should return xml content type when forced', async () => {
+    app.get('/test-content-type-xml', (req, res) => {
+      res.type('xml');
+      res.send();
+    });
+    const response = await request(app).get('/test-content-type-xml');
+
+    expect(response.headers['content-type']).toMatch(/xml/);
+  });
 });
