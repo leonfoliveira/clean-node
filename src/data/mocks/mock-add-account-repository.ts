fb891bcb6def964620c98fdbdfe5bd71ee586772
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-13 22:03:34 -0300

    test: ensure MongodbAccountRepository returns an account on success

--- src/data/mocks/mock-add-account-repository.ts
+++ src/data/mocks/mock-add-account-repository.ts
@@ -1,6 +1,9 @@
 import { AddAccountRepository } from '@/data/protocols';
 import { mockAccountModel } from '@/domain/mocks';
 import { AccountModel } from '@/domain/models';
+import { mockAddAccountDTO } from '@/presentation/mocks';
+
+export const mockAddAccountRepositoryParams = mockAddAccountDTO;
 
 export class AddAccountRepositoryStub implements AddAccountRepository {
   response = mockAccountModel();
