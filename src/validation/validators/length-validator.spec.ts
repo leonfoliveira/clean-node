dae6b22b680f3f0ff8e0bdd1bc9286012340f627
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-16 19:32:26 -0300

    test: ensure LengthValidator returns error if max length is invalid

---
+++ src/validation/validators/length-validator.spec.ts
@@ -0,0 +1,28 @@
+import faker from 'faker';
+
+import { InvalidParamError } from '@/presentation/errors';
+
+import { LengthValidator } from './length-validator';
+
+const makeSut = (field: string, options = { min: 2, max: 3 }): LengthValidator =>
+  new LengthValidator(field, options);
+
+describe('LengthValidator', () => {
+  it('should return error if min length is invalid', () => {
+    const field = faker.database.column();
+    const sut = makeSut(field);
+
+    const error = sut.validate({ [field]: '.' });
+
+    expect(error).toEqual(new InvalidParamError(field));
+  });
+
+  it('should return error if max length is invalid', () => {
+    const field = faker.database.column();
+    const sut = makeSut(field);
+
+    const error = sut.validate({ [field]: '....' });
+
+    expect(error).toEqual(new InvalidParamError(field));
+  });
+});
