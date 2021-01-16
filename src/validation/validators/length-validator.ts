e9bf49b33cc861c481c2f348c7a41fd42cb3cb36
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-16 19:31:54 -0300

    feat: ensure LengthValidator returns error if max length is invalid

--- src/validation/validators/length-validator.ts
+++ src/validation/validators/length-validator.ts
@@ -2,12 +2,21 @@ import { InvalidParamError } from '@/presentation/errors';
 import { Validator } from '@/presentation/interfaces';
 
 export class LengthValidator implements Validator {
-  constructor(private readonly fieldName: string, private readonly options: { min?: number }) {}
+  constructor(
+    private readonly fieldName: string,
+    private readonly options: { min?: number; max?: number },
+  ) {}
 
   validate(input: Record<string, any>): Error {
     if (
       this.options.min &&
       (!input[this.fieldName] || input[this.fieldName].length < this.options.min)
+    ) {
+      return new InvalidParamError(this.fieldName);
+    }
+    if (
+      this.options.max &&
+      (!input[this.fieldName] || input[this.fieldName].length > this.options.max)
     ) {
       return new InvalidParamError(this.fieldName);
     }
