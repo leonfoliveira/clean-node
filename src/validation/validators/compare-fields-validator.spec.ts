bff81556ecb0dd73bb1dcf3a778defd8a41ddd84
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-16 18:54:22 -0300

    test: ensure CompareFieldsValidator returns error if compare is invalid

---
+++ src/validation/validators/compare-fields-validator.spec.ts
@@ -0,0 +1,24 @@
+import faker from 'faker';
+
+import { InvalidParamError } from '@/presentation/errors';
+
+import { CompareFieldsValidator } from './compare-fields-validator';
+
+const makeSut = (field: string, fieldToCompare: string): CompareFieldsValidator =>
+  new CompareFieldsValidator(field, fieldToCompare);
+
+describe('CompareFieldsValidator', () => {
+  test('Should return error if compare is invalid', () => {
+    const field = faker.database.column();
+    const fieldToCompare = `diff_${field}`;
+    const sut = makeSut(field, fieldToCompare);
+
+    const fieldValue = faker.random.words();
+    const error = sut.validate({
+      [field]: fieldValue,
+      [fieldToCompare]: `diff_${fieldValue}`,
+    });
+
+    expect(error).toEqual(new InvalidParamError(field));
+  });
+});
--- src/validation/validators/compare-fields-validator.ts
+++ src/validation/validators/compare-fields-validator.ts
@@ -5,7 +5,7 @@ export class CompareFieldsValidator implements Validator {
   constructor(private readonly fieldName: string, private readonly fieldToCompareName: string) {}
 
   validate(input: Record<string, any>): Error {
-    if (input[this.fieldName] === input[this.fieldToCompareName]) {
+    if (input[this.fieldName] !== input[this.fieldToCompareName]) {
       return new InvalidParamError(this.fieldName);
     }
     return null;
