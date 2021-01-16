7a73609e378d60ae891602bec8245fdedc688965
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-16 15:10:40 -0300

    test: ensure LoginController returns 400 if no email is provided

---
+++ src/presentation/controllers/login-controller.spec.ts
@@ -0,0 +1,26 @@
+import faker from 'faker';
+
+import { HttpRequest, HttpResponse } from '@/presentation/interfaces';
+
+import { MissingParamError } from '../errors';
+
+import { LoginController } from './login-controller';
+
+const mockLoginHttpRequest = (): HttpRequest => ({
+  body: {
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  },
+});
+
+describe('LoginController', () => {
+  it('should return 400 if no email is provided', async () => {
+    const sut = new LoginController();
+    const httpRequest = mockLoginHttpRequest();
+    delete httpRequest.body.email;
+
+    const httpResponse = await sut.handle(mockLoginHttpRequest());
+
+    expect(httpResponse).toEqual(HttpResponse.BadRequest(new MissingParamError('email')));
+  });
+});
