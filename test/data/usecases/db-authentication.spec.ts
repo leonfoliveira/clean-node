068620d19061907cc91126958392cf4f203c0f1e
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-16 20:59:21 -0300

    refactor: move SUT creation to a factory method

--- test/data/usecases/db-authentication.spec.ts
+++ test/data/usecases/db-authentication.spec.ts
@@ -2,10 +2,21 @@ import { DbAuthentication } from '@/data/usecases';
 import { LoadAccountByEmailRepositoryStub } from '@/test/data/mocks';
 import { mockAuthenticationDTO } from '@/test/domain/mocks';
 
+type SutTypes = {
+  sut: DbAuthentication;
+  loadAccountByEmailRepositoryStub: LoadAccountByEmailRepositoryStub;
+};
+
+const makeSut = (): SutTypes => {
+  const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub();
+  const sut = new DbAuthentication(loadAccountByEmailRepositoryStub);
+
+  return { sut, loadAccountByEmailRepositoryStub };
+};
+
 describe('DbAuthentication', () => {
   it('should call LoadAccountByEmailRepository with correct email', async () => {
-    const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub();
-    const sut = new DbAuthentication(loadAccountByEmailRepositoryStub);
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut();
     const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load');
     const authenticationDTO = mockAuthenticationDTO();
 
