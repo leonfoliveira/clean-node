4e20623db715f2be636328e43be08a6f69e1e8f7
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-16 14:47:37 -0300

    feat: apply LogControllerDecorator in SignUpController factory

--- src/main/factories/controllers/signup-controller-factory.ts
+++ src/main/factories/controllers/signup-controller-factory.ts
@@ -1,6 +1,8 @@
+import { makeLogControllerDecorator } from '@/main/factories/decorators';
 import { makeAddAccount } from '@/main/factories/usecases';
 import { makeEmailValidatorAdapter } from '@/main/factories/utils';
 import { SignUpController } from '@/presentation/controllers';
+import { Controller } from '@/presentation/interfaces';
 
-export const makeSignUpController = (): SignUpController =>
-  new SignUpController(makeEmailValidatorAdapter(), makeAddAccount());
+export const makeSignUpController = (): Controller =>
+  makeLogControllerDecorator(new SignUpController(makeEmailValidatorAdapter(), makeAddAccount()));
