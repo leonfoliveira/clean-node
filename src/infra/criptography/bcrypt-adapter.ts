4296846cc6acbed95431fdd66220a5a61b8b510a
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-17 13:05:54 -0300

    feat: ensure BcryptAdapter calls bcrypt.compare with correct values

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,12 +1,17 @@
 import bcrypt from 'bcrypt';
 
-import { HashGenerator } from '@/data/interfaces/criptography';
+import { HashComparer, HashGenerator } from '@/data/interfaces/criptography';
 
-export class BcryptAdapter implements HashGenerator {
+export class BcryptAdapter implements HashGenerator, HashComparer {
   constructor(private readonly rounds: number) {}
 
   async generate(value: string): Promise<string> {
     const hash = await bcrypt.hash(value, this.rounds);
     return hash;
+  }
+
+  async compare(value: string, hash: string): Promise<boolean> {
+    await bcrypt.compare(value, hash);
+    return null;
   }
 }
