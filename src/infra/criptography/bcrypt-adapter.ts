cb3f8ee041a84b9a24f9a59d066b63cf9428a481
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-17 13:10:48 -0300

    refactor: improve legibility

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -6,8 +6,7 @@ export class BcryptAdapter implements HashGenerator, HashComparer {
   constructor(private readonly rounds: number) {}
 
   async generate(value: string): Promise<string> {
-    const hash = await bcrypt.hash(value, this.rounds);
-    return hash;
+    return bcrypt.hash(value, this.rounds);
   }
 
   async compare(value: string, hash: string): Promise<boolean> {
