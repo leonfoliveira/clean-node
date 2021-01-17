3b52fef40e35a959f19481a803a0fa3b13a97885
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-17 17:00:12 -0300

    feat: ensure MongodbAccountRepository return an AccountModel on loadByEmail success

--- src/infra/db/mongodb/mongodb-account-repository.ts
+++ src/infra/db/mongodb/mongodb-account-repository.ts
@@ -1,13 +1,24 @@
-import { AddAccountRepository, AddAccountRepositoryParams } from '@/data/interfaces/db';
+import {
+  AddAccountRepository,
+  AddAccountRepositoryParams,
+  LoadAccountByEmailRepository,
+} from '@/data/interfaces/db';
 import { AccountModel } from '@/domain/models';
 import { MongoHelper } from '@/infra/db/mongodb';
 
-export class MongodbAccountRepository implements AddAccountRepository {
+export class MongodbAccountRepository
+  implements AddAccountRepository, LoadAccountByEmailRepository {
   async add(params: AddAccountRepositoryParams): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts');
-
     const result = await accountCollection.insertOne(params);
 
     return MongoHelper.mapId(result.ops[0]);
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    const account = await accountCollection.findOne({ email });
+
+    return MongoHelper.mapId(account);
   }
 }
