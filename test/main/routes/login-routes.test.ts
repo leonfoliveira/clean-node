c0d86e4c72027f91d5a89cda3aadce0e647f1558
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-17 21:26:51 -0300

    test: ensure login route integration

--- test/main/routes/login-routes.test.ts
+++ test/main/routes/login-routes.test.ts
@@ -1,10 +1,15 @@
+import bcrypt from 'bcrypt';
+import faker from 'faker';
+import { Collection } from 'mongodb';
 import request from 'supertest';
 
 import { MongoHelper } from '@/infra/db/mongodb';
 import { app } from '@/main/config/app';
-import { mockSignupHttpRequest } from '@/test/presentation/mocks';
+import { mockLoginHttpRequest, mockSignupHttpRequest } from '@/test/presentation/mocks';
 
 describe('LoginRoutes', () => {
+  let accountCollection: Collection;
+
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
   });
@@ -14,13 +19,28 @@ describe('LoginRoutes', () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts');
+    accountCollection = await MongoHelper.getCollection('accounts');
     await accountCollection.deleteMany({});
   });
 
   describe('POST /signup', () => {
     it('should return 200 on signup', async () => {
       const response = await request(app).post('/api/signup').send(mockSignupHttpRequest().body);
+
+      expect(response.status).toBe(200);
+    });
+  });
+
+  describe('POST /login', () => {
+    it('should return 200 on login', async () => {
+      const httpRequest = mockLoginHttpRequest();
+      const account = httpRequest.body;
+      await accountCollection.insertOne({
+        ...account,
+        password: await bcrypt.hash(account.password, 12),
+        name: faker.name.findName(),
+      });
+      const response = await request(app).post('/api/login').send(httpRequest.body);
 
       expect(response.status).toBe(200);
     });
