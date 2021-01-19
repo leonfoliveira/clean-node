c2e4ccaa24f126b629ce5335fe07250ae9684595
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-19 17:51:20 -0300

    refactor: move SUT creation to a factory method

--- test/presentation/controllers/survey/add-survey-controller.spec.ts
+++ test/presentation/controllers/survey/add-survey-controller.spec.ts
@@ -3,11 +3,22 @@ import faker from 'faker';
 import { AddSurveyController } from '@/presentation/controllers';
 import { mockAddSurveyHttpRequest, ValidatorStub } from '@/test/presentation/mocks';
 
+type SutTypes = {
+  sut: AddSurveyController;
+  validatorStub: ValidatorStub;
+};
+
+const makeSut = (): SutTypes => {
+  const validatorStub = new ValidatorStub(faker.database.column());
+  const sut = new AddSurveyController(validatorStub);
+
+  return { sut, validatorStub };
+};
+
 describe('AddSurveyController', () => {
   it('should call Validator with correct values', async () => {
-    const validatorStub = new ValidatorStub(faker.database.column());
+    const { sut, validatorStub } = makeSut();
     const validateSpy = jest.spyOn(validatorStub, 'validate');
-    const sut = new AddSurveyController(validatorStub);
     const httpRequest = mockAddSurveyHttpRequest();
 
     await sut.handle(httpRequest);
