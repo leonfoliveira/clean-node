5945a6b2d2e97fb1a95de74dd94fcd77dc9a6f1c
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-19 20:03:00 -0300

    test: ensure SurveyRoutes POST /surveys returns 200 on success

---
+++ test/main/routes/survey-routes.test.ts
@@ -0,0 +1,33 @@
+import { Collection } from 'mongodb';
+import request from 'supertest';
+
+import { MongoHelper } from '@/infra';
+import { app } from '@/main/config/app';
+import { mockAddSurveyHttpRequest } from '@/test/presentation/mocks';
+
+describe('SurveyRoutes', () => {
+  let surveyCollection: Collection;
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('accounts');
+    await surveyCollection.deleteMany({});
+  });
+
+  describe('POST /surveys', () => {
+    it('should return 200 on success', async () => {
+      const httpResponse = await request(app)
+        .post('/api/surveys')
+        .send(mockAddSurveyHttpRequest().body);
+
+      expect(httpResponse.status).toBe(204);
+    });
+  });
+});
