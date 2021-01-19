72a479a2133397abc8111d6b12fd70be850782d3
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-19 16:11:47 -0300

    feat: update AddAccount factory

--- src/main/factories/usecases/add-account-factory.ts
+++ src/main/factories/usecases/add-account-factory.ts
@@ -1,7 +1,11 @@
 import { DbAddAccount } from '@/data/usecases';
 import { AddAccount } from '@/domain/usecases';
 import { makeHashGenerator } from '@/main/factories/cryptography';
-import { makeAddAccountRepository } from '@/main/factories/db';
+import { makeAddAccountRepository, makeLoadAccountByEmailRepository } from '@/main/factories/db';
 
 export const makeAddAccount = (): AddAccount =>
-  new DbAddAccount(makeHashGenerator(), makeAddAccountRepository());
+  new DbAddAccount(
+    makeHashGenerator(),
+    makeAddAccountRepository(),
+    makeLoadAccountByEmailRepository(),
+  );
