0d3efb3cb195fac56b2e69fadcd9444a3360f49f
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-19 18:33:40 -0300

    feat: ensure AddSurveyController returns 500 if AddSurvey throws

--- src/presentation/controllers/survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey-controller.ts
@@ -6,13 +6,18 @@ export class AddSurveyController implements Controller {
   constructor(private readonly validator: Validator, private readonly addSurvey: AddSurvey) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validator.validate(httpRequest.body);
-    if (error) {
-      return HttpResponseFactory.makeBadRequest(error);
+    try {
+      const error = this.validator.validate(httpRequest.body);
+      if (error) {
+        return HttpResponseFactory.makeBadRequest(error);
+      }
+
+      const { question, answers } = httpRequest.body;
+      await this.addSurvey.add({ question, answers });
+
+      return null;
+    } catch (error) {
+      return HttpResponseFactory.makeInternalServerError(error);
     }
-
-    await this.addSurvey.add(httpRequest.body);
-
-    return null;
   }
 }
