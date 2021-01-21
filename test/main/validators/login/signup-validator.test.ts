9a973189c86365b3a48337cb7ed3007d58358801
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-21 12:15:28 -0300

    test: ensure SignUpValidator validates email field

---
+++ test/main/validators/login/signup-validator.test.ts
@@ -0,0 +1,46 @@
+import faker from 'faker';
+import { Collection } from 'mongodb';
+import request from 'supertest';
+
+import { MongoHelper } from '@/infra';
+import { app } from '@/main/config/app';
+import { testInvalidParamResponse } from '@/test/helpers';
+import { mockLoginHttpRequest } from '@/test/presentation/mocks/http-requests';
+
+describe('SignUpValidator', () => {
+  let accountCollection: Collection;
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  });
+
+  it('should return 400 if no email is provided', async () => {
+    const httpRequest = mockLoginHttpRequest();
+    delete httpRequest.body.email;
+    const httpResponse = await request(app).post('/api/signup').send(httpRequest.body);
+    testInvalidParamResponse(httpResponse);
+  });
+
+  it('should return 400 if email is not a string', async () => {
+    const httpRequest = mockLoginHttpRequest();
+    httpRequest.body.email = faker.random.number();
+    const httpResponse = await request(app).post('/api/signup').send(httpRequest.body);
+    testInvalidParamResponse(httpResponse);
+  });
+
+  it('should return 400 if email is an invalid email address', async () => {
+    const httpRequest = mockLoginHttpRequest();
+    httpRequest.body.email = faker.random.word();
+    const httpResponse = await request(app).post('/api/signup').send(httpRequest.body);
+    testInvalidParamResponse(httpResponse);
+  });
+});
