36664f49650819206fe9c9c870636977f1f10019
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-21 19:15:47 -0300

    refactor: move SUT creation to a factory method

--- test/data/usecases/survey/db-load-surveys.spec.ts
+++ test/data/usecases/survey/db-load-surveys.spec.ts
@@ -1,11 +1,22 @@
 import { DbLoadSurveys } from '@/data/usecases';
 import { LoadSurveysRepositoryStub } from '@/test/data/mocks';
 
+type SutTypes = {
+  sut: DbLoadSurveys;
+  loadSurveysRepositoryStub: LoadSurveysRepositoryStub;
+};
+
+const makeSut = (): SutTypes => {
+  const loadSurveysRepositoryStub = new LoadSurveysRepositoryStub();
+  const sut = new DbLoadSurveys(loadSurveysRepositoryStub);
+
+  return { sut, loadSurveysRepositoryStub };
+};
+
 describe('DbLoadSurveys', () => {
   it('should call LoadSurveysRepository', async () => {
-    const loadSurveysRepositoryStub = new LoadSurveysRepositoryStub();
+    const { sut, loadSurveysRepositoryStub } = makeSut();
     const loadAllSpy = jest.spyOn(loadSurveysRepositoryStub, 'loadAll');
-    const sut = new DbLoadSurveys(loadSurveysRepositoryStub);
 
     await sut.load();
 
