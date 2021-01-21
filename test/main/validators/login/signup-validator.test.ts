3b9e185cb817848ca2d1d76a34f996e5694fe0a2
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-21 12:17:43 -0300

    test: ensure SignUpValidator validates name field

--- test/main/validators/login/signup-validator.test.ts
+++ test/main/validators/login/signup-validator.test.ts
@@ -23,6 +23,34 @@ describe('SignUpValidator', () => {
     await accountCollection.deleteMany({});
   });
 
+  it('should return 400 if no name is provided', async () => {
+    const httpRequest = mockLoginHttpRequest();
+    delete httpRequest.body.name;
+    const httpResponse = await request(app).post('/api/signup').send(httpRequest.body);
+    testInvalidParamResponse(httpResponse);
+  });
+
+  it('should return 400 if name is not a string', async () => {
+    const httpRequest = mockLoginHttpRequest();
+    httpRequest.body.name = faker.random.number();
+    const httpResponse = await request(app).post('/api/signup').send(httpRequest.body);
+    testInvalidParamResponse(httpResponse);
+  });
+
+  it('should return 400 if name is empty', async () => {
+    const httpRequest = mockLoginHttpRequest();
+    httpRequest.body.name = '';
+    const httpResponse = await request(app).post('/api/signup').send(httpRequest.body);
+    testInvalidParamResponse(httpResponse);
+  });
+
+  it('should return 400 if name length is more than 50', async () => {
+    const httpRequest = mockLoginHttpRequest();
+    httpRequest.body.name = [...Array.from({ length: 33 }).keys()].join('');
+    const httpResponse = await request(app).post('/api/signup').send(httpRequest.body);
+    testInvalidParamResponse(httpResponse);
+  });
+
   it('should return 400 if no email is provided', async () => {
     const httpRequest = mockLoginHttpRequest();
     delete httpRequest.body.email;
