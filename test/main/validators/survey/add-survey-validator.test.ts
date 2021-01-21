b22d2ff197bb5f472f185a1f8a48ae280f83ba00
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-21 13:10:41 -0300

    test: ensure AddSurveyValidator validates answers field

--- test/main/validators/survey/add-survey-validator.test.ts
+++ test/main/validators/survey/add-survey-validator.test.ts
@@ -5,7 +5,7 @@ import request from 'supertest';
 import { MongoHelper } from '@/infra';
 import { app } from '@/main/config/app';
 import { testInvalidParamResponse, generateStringWithLength } from '@/test/helpers';
-import { mockSignupHttpRequest } from '@/test/presentation/mocks/http-requests';
+import { mockAddSurveyHttpRequest } from '@/test/presentation/mocks/http-requests';
 
 describe('AddSurveyValidator', () => {
   let surveyCollection: Collection;
@@ -24,29 +24,99 @@ describe('AddSurveyValidator', () => {
   });
 
   it('should return 400 if no question is provided', async () => {
-    const httpRequest = mockSignupHttpRequest();
+    const httpRequest = mockAddSurveyHttpRequest();
     delete httpRequest.body.question;
     const httpResponse = await request(app).post('/api/surveys').send(httpRequest.body);
     testInvalidParamResponse(httpResponse);
   });
 
   it('should return 400 if question is not a string', async () => {
-    const httpRequest = mockSignupHttpRequest();
+    const httpRequest = mockAddSurveyHttpRequest();
     httpRequest.body.question = faker.random.number();
     const httpResponse = await request(app).post('/api/surveys').send(httpRequest.body);
     testInvalidParamResponse(httpResponse);
   });
 
   it('should return 400 if question is empty', async () => {
-    const httpRequest = mockSignupHttpRequest();
+    const httpRequest = mockAddSurveyHttpRequest();
     httpRequest.body.question = '';
     const httpResponse = await request(app).post('/api/surveys').send(httpRequest.body);
     testInvalidParamResponse(httpResponse);
   });
 
   it('should return 400 if question length is more than 100', async () => {
-    const httpRequest = mockSignupHttpRequest();
+    const httpRequest = mockAddSurveyHttpRequest();
     httpRequest.body.question = generateStringWithLength(101);
+    const httpResponse = await request(app).post('/api/surveys').send(httpRequest.body);
+    testInvalidParamResponse(httpResponse);
+  });
+
+  it('should return 400 if answers is not provided', async () => {
+    const httpRequest = mockAddSurveyHttpRequest();
+    delete httpRequest.body.answers;
+    const httpResponse = await request(app).post('/api/surveys').send(httpRequest.body);
+    testInvalidParamResponse(httpResponse);
+  });
+
+  it('should return 400 if answers is not an array', async () => {
+    const httpRequest = mockAddSurveyHttpRequest();
+    httpRequest.body.answers = faker.random.words();
+    const httpResponse = await request(app).post('/api/surveys').send(httpRequest.body);
+    testInvalidParamResponse(httpResponse);
+  });
+
+  it('should return 400 if answers is not an array of objects', async () => {
+    const httpRequest = mockAddSurveyHttpRequest();
+    httpRequest.body.answers = [faker.random.words()];
+    const httpResponse = await request(app).post('/api/surveys').send(httpRequest.body);
+    testInvalidParamResponse(httpResponse);
+  });
+
+  it('should return 400 if answers.answer is not provided', async () => {
+    const httpRequest = mockAddSurveyHttpRequest();
+    delete httpRequest.body.answers[0].answer;
+    const httpResponse = await request(app).post('/api/surveys').send(httpRequest.body);
+    testInvalidParamResponse(httpResponse);
+  });
+
+  it('should return 400 if answers.answer is not a string', async () => {
+    const httpRequest = mockAddSurveyHttpRequest();
+    httpRequest.body.answers[0].answer = 1;
+    const httpResponse = await request(app).post('/api/surveys').send(httpRequest.body);
+    testInvalidParamResponse(httpResponse);
+  });
+
+  it('should return 400 if answers.answer is empty', async () => {
+    const httpRequest = mockAddSurveyHttpRequest();
+    httpRequest.body.answers[0].answer = '';
+    const httpResponse = await request(app).post('/api/surveys').send(httpRequest.body);
+    testInvalidParamResponse(httpResponse);
+  });
+
+  it('should return 400 if answers.answer length is more than 100', async () => {
+    const httpRequest = mockAddSurveyHttpRequest();
+    httpRequest.body.answers[0].answer = generateStringWithLength(101);
+    const httpResponse = await request(app).post('/api/surveys').send(httpRequest.body);
+    testInvalidParamResponse(httpResponse);
+  });
+
+  it('should return 400 if answers.image is not a string', async () => {
+    const httpRequest = mockAddSurveyHttpRequest();
+    httpRequest.body.answers[0].image = 0;
+    const httpResponse = await request(app).post('/api/surveys').send(httpRequest.body);
+    testInvalidParamResponse(httpResponse);
+  });
+
+  it('should return 400 if answers.image is empty', async () => {
+    const httpRequest = mockAddSurveyHttpRequest();
+    httpRequest.body.answers[0].image = '';
+    const httpResponse = await request(app).post('/api/surveys').send(httpRequest.body);
+    testInvalidParamResponse(httpResponse);
+  });
+
+  it('should return 400 if answers.image not a valid URL', async () => {
+    const httpRequest = mockAddSurveyHttpRequest();
+    httpRequest.body.answers[0].image = faker.random.words();
     const httpResponse = await request(app).post('/api/surveys').send(httpRequest.body);
     testInvalidParamResponse(httpResponse);
   });
