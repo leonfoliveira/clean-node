f93b252728b2c830328fd1b4f8042f6292ad0815
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-20 21:49:22 -0300

    refactor: move SUT creation to a factory method

--- test/data/usecases/account/db-load-account-by-token.spec.ts
+++ test/data/usecases/account/db-load-account-by-token.spec.ts
@@ -2,11 +2,22 @@ import { DbLoadAccountByToken } from '@/data/usecases';
 import { TokenDecoderStub } from '@/test/data/mocks';
 import { mockLoadAccountByTokenDTO } from '@/test/domain/mocks/usecases';
 
+type SutTypes = {
+  sut: DbLoadAccountByToken;
+  tokenDecoderStub: TokenDecoderStub;
+};
+
+const makeSut = (): SutTypes => {
+  const tokenDecoderStub = new TokenDecoderStub();
+  const sut = new DbLoadAccountByToken(tokenDecoderStub);
+
+  return { sut, tokenDecoderStub };
+};
+
 describe('DbLoadAccountByToken', () => {
   it('should call TokenDecoder with correct values', async () => {
-    const tokenDecoderStub = new TokenDecoderStub();
+    const { sut, tokenDecoderStub } = makeSut();
     const decodeSpy = jest.spyOn(tokenDecoderStub, 'decode');
-    const sut = new DbLoadAccountByToken(tokenDecoderStub);
     const params = mockLoadAccountByTokenDTO();
 
     await sut.load(params);
