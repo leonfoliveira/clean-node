85b5956bfd198e9317ea43ab03d5c23bf07b6769
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-21 18:53:46 -0300

    refactor: move SUT to a factory method

--- test/presentation/controllers/survey/load-surveys-controller.spec.ts
+++ test/presentation/controllers/survey/load-surveys-controller.spec.ts
@@ -1,11 +1,22 @@
 import { LoadSurveysController } from '@/presentation/controllers';
 import { LoadSurveysStub } from '@/test/domain/mocks/usecases';
 
+type SutTypes = {
+  sut: LoadSurveysController;
+  loadSurveysStub: LoadSurveysStub;
+};
+
+const makeSut = (): SutTypes => {
+  const loadSurveysStub = new LoadSurveysStub();
+  const sut = new LoadSurveysController(loadSurveysStub);
+
+  return { sut, loadSurveysStub };
+};
+
 describe('LoadSurveysController', () => {
   it('should call LoadSurveys', async () => {
-    const loadSurveysStub = new LoadSurveysStub();
+    const { sut, loadSurveysStub } = makeSut();
     const loadSpy = jest.spyOn(loadSurveysStub, 'load');
-    const sut = new LoadSurveysController(loadSurveysStub);
 
     await sut.handle();
 
