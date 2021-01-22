135776076757ae31fb0b5bf6a6ed8213cb74c945
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-22 16:26:20 -0300

    feat: ensure MongodbSurveyRepository loads survey by id on success

--- src/infra/db/mongodb-survey-repository.ts
+++ src/infra/db/mongodb-survey-repository.ts
@@ -2,11 +2,13 @@ import {
   AddSurveyRepository,
   AddSurveyRepositoryParams,
   LoadSurveysRepository,
+  LoadSurveyByIdRepository,
 } from '@/data/interfaces';
 import { SurveyModel } from '@/domain/models';
 import { MongoHelper } from '@/infra';
 
-export class MongodbSurveyRepository implements AddSurveyRepository, LoadSurveysRepository {
+export class MongodbSurveyRepository
+  implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
   async add(params: AddSurveyRepositoryParams): Promise<SurveyModel> {
     const surveyCollection = await MongoHelper.getCollection('surveys');
     const survey = await surveyCollection.insertOne(params);
@@ -19,5 +21,12 @@ export class MongodbSurveyRepository implements AddSurveyRepository, LoadSurveys
     const surveys = await surveyCollection.find().toArray();
 
     return surveys.map((survey) => MongoHelper.mapId(survey));
+  }
+
+  async loadById(id: string): Promise<SurveyModel> {
+    const surveyCollection = await MongoHelper.getCollection('surveys');
+    const survey = await surveyCollection.findOne({ _id: id });
+
+    return survey;
   }
 }
