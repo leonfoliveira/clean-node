9a2e727e8fe0d0437f486ff8331dc295c5eec9e3
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-22 16:45:24 -0300

    test: ensure MongodbSurveyResultRepository returns a SurveyResult on success

---
+++ test/infra/db/mongodb-survey-result-repository.spec.ts
@@ -0,0 +1,46 @@
+import { Collection } from 'mongodb';
+
+import { MongodbSurveyResultRepository, MongoHelper } from '@/infra';
+import { mockSurveyResultModel } from '@/test/domain/mocks/models';
+
+const makeSut = (): MongodbSurveyResultRepository => new MongodbSurveyResultRepository();
+
+describe('MongodbSurveyResultRepository', () => {
+  let surveyCollection: Collection;
+  let surveyResultCollection: Collection;
+  let accountCollection: Collection;
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys');
+    await surveyCollection.deleteMany({});
+    surveyResultCollection = await MongoHelper.getCollection('surveyResults');
+    await surveyResultCollection.deleteMany({});
+    accountCollection = await MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  });
+
+  describe('SaveSurveyResultRepository', () => {
+    it('should return a SurveyResult on success', async () => {
+      const sut = makeSut();
+      const params = mockSurveyResultModel();
+      delete params.id;
+
+      const result = await sut.save(params);
+
+      expect(result).toBeTruthy();
+      expect(result.id).toBeTruthy();
+      expect(result.surveyId).toBe(params.surveyId);
+      expect(result.accountId).toBe(params.accountId);
+      expect(result.answer).toEqual(params.answer);
+      expect(result.date).toEqual(params.date);
+    });
+  });
+});
