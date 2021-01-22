2e1c30d91b4f5029d52dd057e1fc1e50a5c996cf
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-22 19:46:54 -0300

    feat: ensure SaveSurveyResultController returns 500 if LoadSurveyById throws

--- src/presentation/controllers/survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result-controller.ts
@@ -7,8 +7,12 @@ export class SaveSurveyResultController implements Controller {
   constructor(private readonly loadSurveyById: LoadSurveyById) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { surveyId } = httpRequest.params;
-    await this.loadSurveyById.loadById(surveyId);
-    return HttpResponseFactory.makeNotFound(new RegisterNotFoundError('survey'));
+    try {
+      const { surveyId } = httpRequest.params;
+      await this.loadSurveyById.loadById(surveyId);
+      return HttpResponseFactory.makeNotFound(new RegisterNotFoundError('survey'));
+    } catch (error) {
+      return HttpResponseFactory.makeInternalServerError(error);
+    }
   }
 }
