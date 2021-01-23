f4a7c9b859c6e09930b816b5d18c2a97b0cfd627
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-23 15:47:17 -0300

    docs: add swagger login-path

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,10 +1,39 @@
-import pjson from '@/../package.json';
+import badRequestComponent from './components/bad-request.json';
+import internalServerErrorComponent from './components/internal-server-error.json';
+import notFoundComponent from './components/not-found.json';
+import unauthorizedComponent from './components/unauthorized.json';
+import loginPath from './paths/login-path.json';
+import authorizationSchema from './schemas/authorization-schema.json';
+import errorSchema from './schemas/error-schema.json';
 
 export const swaggerConfig = {
   openapi: '3.0.0',
   info: {
     title: 'Fordevs API',
     description: 'Fordevs, surveys for programmers API',
-    version: pjson.version,
+    version: '1.0.0',
+  },
+  servers: [
+    {
+      url: '/api',
+    },
+  ],
+  tags: [
+    {
+      name: 'Login',
+    },
+  ],
+  components: {
+    'bad-request': badRequestComponent,
+    'internal-server-error': internalServerErrorComponent,
+    'not-found': notFoundComponent,
+    unauthorized: unauthorizedComponent,
+  },
+  paths: {
+    '/login': loginPath,
+  },
+  schemas: {
+    error: errorSchema,
+    authorization: authorizationSchema,
   },
 };
