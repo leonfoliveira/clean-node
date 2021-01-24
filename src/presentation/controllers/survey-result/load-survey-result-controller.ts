4d4831bbbfee8f1e2a567365d6c6a97a1235500e
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-24 18:06:54 -0300

    feat: ensure LoadSurveyResultController calls LoadSurveyById with correct values

--- src/presentation/controllers/survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result-controller.ts
@@ -1,13 +1,20 @@
-import { LoadSurveyResult } from '@/domain/usecases';
+import { LoadSurveyById, LoadSurveyResult } from '@/domain/usecases';
 import { RegisterNotFoundError } from '@/presentation/errors';
 import { HttpResponseFactory } from '@/presentation/helpers';
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/interfaces';
 
 export class LoadSurveyResultController implements Controller {
-  constructor(private readonly loadSurveyResult: LoadSurveyResult) {}
+  constructor(
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly loadSurveyResult: LoadSurveyResult,
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      await this.loadSurveyById.loadById({
+        id: httpRequest.params.surveyId,
+      });
+
       await this.loadSurveyResult.load({
         surveyId: httpRequest.params.surveyId,
         accountId: httpRequest.headers.accountId,
