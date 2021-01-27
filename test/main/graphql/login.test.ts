deaa323da962ac3408e54144cfdb4c4f47056a57
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-27 15:24:42 -0300

    test: ensure SignUp Mutation returns an Authorization on valid data

--- test/main/graphql/login.test.ts
+++ test/main/graphql/login.test.ts
@@ -7,6 +7,7 @@ import { Collection } from 'mongodb';
 import { MongoHelper } from '@/infra';
 import { UnauthorizedError } from '@/presentation/errors';
 import { mockAccountEntity } from '@/test/infra/mocks';
+import { mockSignupRequest } from '@/test/presentation/mocks';
 
 import { makeApolloServer } from './helpers';
 
@@ -69,6 +70,39 @@ describe('LoginGraphQL', () => {
 
       expect(result.data).toBeFalsy();
       expect(result.errors[0].message).toBe(new UnauthorizedError().message);
+    });
+  });
+
+  describe('SignUp Mutation', () => {
+    const signUpMutation = gql`
+      mutation signUp(
+        $name: String!
+        $email: String!
+        $password: String!
+        $passwordConfirmation: String!
+      ) {
+        signUp(
+          name: $name
+          email: $email
+          password: $password
+          passwordConfirmation: $passwordConfirmation
+        ) {
+          accessToken
+          name
+        }
+      }
+    `;
+
+    it('should return an Authorization on valid data', async () => {
+      const request = mockSignupRequest();
+      const { mutate } = createTestClient({ apolloServer });
+
+      const result: any = await mutate(signUpMutation, {
+        variables: request,
+      });
+
+      expect(result.data.signUp.accessToken).toBeTruthy();
+      expect(result.data.signUp.name).toBe(request.name);
     });
   });
 });
