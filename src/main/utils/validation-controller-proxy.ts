a2dce7a12fbe5099585f408e7dd0963354fb0052
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-27 11:56:54 -0300

    feat: ensure ValidationControllerProxy returns 400 if Validator returns an error

--- src/main/utils/validation-controller-proxy.ts
+++ src/main/utils/validation-controller-proxy.ts
@@ -1,11 +1,16 @@
 import { Validator } from '@/data/interfaces';
+import { InvalidParamError } from '@/presentation/errors';
+import { HttpResponseFactory } from '@/presentation/helpers';
 import { Controller } from '@/presentation/interfaces';
 
 export class ValidationControllerProxy implements Controller {
   constructor(private readonly validator: Validator) {}
 
   async handle(request: Record<string, any>): Promise<any> {
-    this.validator.validate(request);
+    const error = this.validator.validate(request);
+    if (error) {
+      return HttpResponseFactory.makeBadRequest(new InvalidParamError(error.message));
+    }
     return null;
   }
 }
