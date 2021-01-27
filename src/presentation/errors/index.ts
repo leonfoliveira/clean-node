52d87c7829a252f2bfa8bc54866d18dd92ca329f
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-26 23:55:31 -0300

    feat: ensure JoiValidatorAdapter returns InvalidParamError if joi.validate returns an error

--- src/presentation/errors/index.ts
+++ src/presentation/errors/index.ts
@@ -1,5 +1,6 @@
 export * from './access-denied-error';
 export * from './email-in-use-error';
+export * from './invalid-param-error';
 export * from './register-not-found-error';
 export * from './server-error';
 export * from './unauthorized-error';
--- src/validation/joi-validator-adapter.ts
+++ src/validation/joi-validator-adapter.ts
@@ -1,12 +1,16 @@
 import { Schema } from 'joi';
 
+import { InvalidParamError } from '@/presentation/errors';
 import { Validator } from '@/presentation/interfaces';
 
 export class JoiValidatorAdapter implements Validator {
   constructor(private readonly schema: Schema) {}
 
   validate(data: Record<string, any>): Error {
-    this.schema.validate(data);
+    const result = this.schema.validate(data);
+    if (result.error) {
+      return new InvalidParamError(result.error.message);
+    }
 
     return null;
   }
