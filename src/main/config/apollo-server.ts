50c06bf9ad0b70797fc7ca2ece311cd317b7468f
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-26 21:58:20 -0300

    feat: add auth directive to surveys query

--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -7,6 +7,7 @@ import {
 import { Application } from 'express';
 import { GraphQLError } from 'graphql';
 
+import schemaDirectives from '@/main/graphql/directives';
 import resolvers from '@/main/graphql/resolvers';
 import typeDefs from '@/main/graphql/type-defs';
 
@@ -32,6 +33,7 @@ export const setupApolloServer = (app: Application): void => {
   const server = new ApolloServer({
     resolvers,
     typeDefs,
+    context: ({ req }): any => ({ req }),
     plugins: [
       {
         requestDidStart: (): Record<string, any> => ({
@@ -39,6 +41,7 @@ export const setupApolloServer = (app: Application): void => {
         }),
       },
     ],
+    schemaDirectives,
   });
 
   server.applyMiddleware({ app });
---
+++ src/main/graphql/directives/auth-directive.ts
@@ -0,0 +1,24 @@
+import { ForbiddenError, SchemaDirectiveVisitor } from 'apollo-server-express';
+import { defaultFieldResolver, GraphQLField } from 'graphql';
+
+import { makeAuthMiddleware } from '@/main/factories/middlewares';
+
+export class AuthDirective extends SchemaDirectiveVisitor {
+  visitFieldDefinition(field: GraphQLField<any, any>): any {
+    const { resolve = defaultFieldResolver } = field;
+    // eslint-disable-next-line no-param-reassign
+    field.resolve = async (parent, args, context, info): Promise<any> => {
+      const request: any = {
+        accessToken: context?.req?.headers?.['x-access-token'],
+      };
+
+      const httpResponse = await makeAuthMiddleware().handle(request);
+
+      if (httpResponse.statusCode === 200) {
+        Object.assign(context?.req, httpResponse.body);
+        return resolve.call(this, parent, args, context, info);
+      }
+      throw new ForbiddenError(httpResponse.body.message);
+    };
+  }
+}
