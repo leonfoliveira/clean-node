cdb62ea682196ac78e84b9dc3ceaa4d719a03063
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-27 16:35:54 -0300

    feat: ensure SurveyResult Query returns SurveyResult on success

---
+++ test/main/graphql/survey-result.test.ts
@@ -0,0 +1,81 @@
+import { ApolloServer, gql } from 'apollo-server-express';
+import { createTestClient } from 'apollo-server-integration-testing';
+import jwt from 'jsonwebtoken';
+import { Collection } from 'mongodb';
+
+import { MongoHelper } from '@/infra';
+import { env } from '@/main/config';
+import { mockAccountEntity, mockSurveyEntity } from '@/test/infra/mocks';
+
+import { makeApolloServer } from './helpers';
+
+let accountCollection: Collection;
+let surveyCollection: Collection;
+let apolloServer: ApolloServer;
+
+const makeAccessToken = async (role?: string): Promise<string> => {
+  const account = mockAccountEntity(role);
+  const result = (await accountCollection.insertOne(account)).ops[0];
+
+  const accessToken = jwt.sign({ id: result._id }, env.jwtSecret);
+  await accountCollection.updateOne({ _id: result._id }, { $set: { accessToken } });
+  return accessToken;
+};
+
+describe('SurveyResultGraphQL', () => {
+  beforeAll(async () => {
+    apolloServer = makeApolloServer();
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts');
+    surveyCollection = await MongoHelper.getCollection('surveys');
+    await accountCollection.deleteMany({});
+    await surveyCollection.deleteMany({});
+  });
+
+  describe('SurveyResult Query', () => {
+    const surveyResultQuery = gql`
+      query surveyResult($surveyId: String!) {
+        surveyResult(surveyId: $surveyId) {
+          question
+          answers {
+            answer
+            count
+            percent
+            isCurrentAccountAnswer
+          }
+          date
+        }
+      }
+    `;
+
+    it('should return SurveyResult on success', async () => {
+      const survey = mockSurveyEntity();
+      const surveyId = (await surveyCollection.insertOne(survey)).ops[0]._id.toString();
+      const accessToken = await makeAccessToken();
+      const { query } = createTestClient({
+        apolloServer,
+        extendMockRequest: { headers: { 'x-access-token': accessToken } },
+      });
+
+      const result: any = await query(surveyResultQuery, { variables: { surveyId } });
+
+      expect(result.data.surveyResult.question).toEqual(survey.question);
+      expect(result.data.surveyResult.answers).toEqual(
+        survey.answers.map((a: any) => ({
+          answer: a.answer,
+          count: 0,
+          percent: 0,
+          isCurrentAccountAnswer: null,
+        })),
+      );
+      expect(result.data.surveyResult.date).toBe(survey.date.toISOString());
+    });
+  });
+});
