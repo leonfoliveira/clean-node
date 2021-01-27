e4be596d0decf27432ec664726874c80b562e69a
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-01-27 14:34:08 -0300

    feat: add surveyResult query and mutation

---
+++ src/main/graphql/resolvers/survey-result.ts
@@ -0,0 +1,17 @@
+import { adaptResolver } from '@/main/adapters';
+import {
+  makeLoadSurveyResultController,
+  makeSaveSurveyResultController,
+} from '@/main/factories/controllers';
+
+export default {
+  Query: {
+    surveyResult: async (parent: any, args: any): Promise<any> =>
+      adaptResolver(makeLoadSurveyResultController(), args),
+  },
+
+  Mutation: {
+    saveSurveyResult: async (parent: any, args: any): Promise<any> =>
+      adaptResolver(makeSaveSurveyResultController(), args),
+  },
+};
